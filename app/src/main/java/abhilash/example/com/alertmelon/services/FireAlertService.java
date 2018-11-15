09e01f9be7d4724ada4d4d4ac74113d5ef4d88e8
AbhilashG97 <abhilash.ag1997@gmail.com>
2018-11-15 15:07:54 +0530

    minor changes made to notifications

--- app/src/main/java/abhilash/example/com/alertmelon/services/FireAlertService.java
+++ app/src/main/java/abhilash/example/com/alertmelon/services/FireAlertService.java
@@ -11,6 +11,7 @@ import android.graphics.Color;
 import android.os.Build;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -18,6 +19,7 @@ import com.pubnub.api.PubNub;
 
 import abhilash.example.com.alertmelon.MainActivity;
 import abhilash.example.com.alertmelon.R;
+import abhilash.example.com.alertmelon.activities.NotificationActivity;
 import abhilash.example.com.alertmelon.singleton.PubNubSingleton;
 
 public class FireAlertService extends Service {
@@ -29,6 +31,7 @@ public class FireAlertService extends Service {
     @Override
     public void onCreate() {
         mPubNub = PubNubSingleton.getInstance();
+        createAlertNotification();
     }
 
     /**
@@ -44,6 +47,13 @@ public class FireAlertService extends Service {
         Log.i("SERVICE STARTED", "Service started");
         createServiceNotification();
         return START_STICKY;
+    }
+
+    /**
+     * Listener for PubNub singleton instance
+     */
+    public void addPubNubListener() {
+        PubNubSingleton.addListener(mPubNub);
     }
 
     /**
@@ -83,29 +93,56 @@ public class FireAlertService extends Service {
         mBuilder.setContentIntent(contentIntent);
         Notification notification = mBuilder.build();
         notification.flags = notification.flags | Notification.FLAG_NO_CLEAR;
-        mBuilder.setSmallIcon(R.drawable.ic_notification_warning)
-                .setContentTitle("Temperature Alert")
-                .setTicker(getResources().getString(R.string.app_name))
-                .setContentText("Temperature has reached exceedingly high!")
-                .setAutoCancel(true)
+        mBuilder.setAutoCancel(true)
                 .setContentIntent(contentIntent)
                 .setOngoing(true);
         startForeground(NOTIFICATION_ID, notification);
     }
 
     /**
-     * Notifies the user when the temperature reaches abnormally hig
+     * Notifies the user when the temperature reaches abnormally high
      */
     public void createAlertNotification() {
+        android.support.v4.app.NotificationCompat.Builder mBuilder;
 
+        NotificationManager notificationManager = (NotificationManager) this
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String chanel_id = "3000";
+            CharSequence name = "Fire Alert Channel";
+            String description = "Sends a notification when the temperature becomes exceedingly high";
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel mChannel = new NotificationChannel(chanel_id, name, importance);
+            mChannel.setDescription(description);
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.BLUE);
+            notificationManager.createNotificationChannel(mChannel);
+            mBuilder = new android.support.v4.app.NotificationCompat.Builder(this, chanel_id);
+        } else {
+            mBuilder = new android.support.v4.app.NotificationCompat.Builder(this);
+        }
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,
+                0,
+                notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        PendingIntent dismissIntent = NotificationActivity.getDismissIntent(1001,
+                this);
+
+        mBuilder.setContentIntent(contentIntent);
+        mBuilder.setSmallIcon(R.drawable.ic_notification_warning)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentTitle("Temperature Alert")
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setTicker(getResources().getString(R.string.app_name))
+                .setContentText("Temperature has reached exceedingly high!")
+                .setOngoing(true)
+                .addAction(R.drawable.ic_notification_warning, "DISMISS", dismissIntent);
+        notificationManager.notify(1001, mBuilder.build());
     }
 
-    /**
-     * Listener for PubNub singleton
-     */
-    public void addPubNubListener() {
-        PubNubSingleton.addListener(mPubNub);
-    }
 
     @Override
     public void onDestroy() {
